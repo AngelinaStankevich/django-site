1fc349d7399df41dd1027ee4008caafad308f08a
StankevichAngelina <mashianaakalina57@mail.ru>
2022-06-12 22:32:30 +0300

    work on backend

--- lab34/taskmanager/apps/furniture/views.py
+++ lab34/taskmanager/apps/furniture/views.py
@@ -2,4 +2,8 @@ from django.http import HttpResponse
 
 
 def index(request):  # HttpRequest
-    return HttpResponse("Страница furniture.")
+    return HttpResponse("Страница мебели.")
+
+
+def categories(request):
+    return HttpResponse("<h1> Мебель по категориям</h1>")
--- lab34/taskmanager/taskmanager/urls.py
+++ lab34/taskmanager/taskmanager/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from apps.furniture.views import index
+from furniture.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('furniture/', index)
+    path('furniture/', index),
+    path('cats/', categories),
 ]
